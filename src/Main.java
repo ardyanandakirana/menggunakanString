bd68b258cdb7f45f8f4b45442d54a0afd4b4cc96
Biodata <ardya.nanda@raharja.info>
2023-06-06 12:09:33 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+package MenggunakanString;
+public class Main {
+    public static void main(String[] args) {
+        String txt = "Nama : Kurniasih";
+        String txt1 = "Jurusan : Teknik Informatika";
+        String txt2 = "Mata Kuliah : Pemrograman 2";
+        String txt3 = "Kode Kelas : FS204E";
+
+        System.out.println(txt.toUpperCase());
+        System.out.println(txt.toLowerCase());
+
+        System.out.println(txt1.toUpperCase());
+        System.out.println(txt1.toLowerCase());
+
+        System.out.println(txt2.toUpperCase());
+        System.out.println(txt2.toLowerCase());
+
+        System.out.println(txt3.toUpperCase());
+        System.out.println(txt3.toLowerCase());
+
+
+    }
+}
